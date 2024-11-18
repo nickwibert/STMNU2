7dd1f7cd9d905462bab887cd8440de86ff64405b
Nick Wibert <nickwibert@gmail.com>
2024-11-18 11:06:45 -0500

    Update main.py
    Update filepath to reflect new organization structure

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from student_df import StudentDataFrame
 
 def main():
    # Load student info
-   students = StudentDataFrame('C:\\STMNU2\\Data\\STUD00.csv')
+   students = StudentDataFrame('C:\\STMNU2\\data\\dbf_format\\STUD00.csv')
+   # Initialize instance of program
    stmnu = gui.STMNU(students)
+   # Start program loop
    stmnu.mainloop()
 
 if __name__ == "__main__":
    main()
+   
