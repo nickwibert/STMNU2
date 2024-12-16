a29f66314bde71cc48e4fb85e2dcbe9bb8cf9389
Nick Wibert <nickwibert@gmail.com>
2024-12-16 11:09:26 -0500

    Create function `update_class_info`
    Mimics function `update_student_info` but for information about classes (i.e. trials, waitlist, etc.) which come from `clsbymon.dbf`

--- database.py
+++ database.py
@@ -9,6 +9,14 @@ class StudentDatabase:
         self.student_dbf = dbf.Table(student_dbf_path)
         # DBF Table object for clsbymon.dbf
         self.classes_dbf = dbf.Table(clsbymon_dbf_path)
+        # Add 'CLASS_ID' to clsbymon DBF file (if it doesn't exist)
+        with self.classes_dbf:
+            if 'CLASS_ID' not in self.classes_dbf.field_names:
+                self.classes_dbf.add_fields('CLASS_ID N(3,0)')
+                class_id = 1
+                for record in self.classes_dbf:
+                    dbf.write(record, CLASS_ID=class_id)
+                    class_id += 1 
 
     def load_data(self):
         # Transform current versions of DBF files to CSV
@@ -18,7 +26,7 @@ class StudentDatabase:
         fn.transform_to_rdb(data_path='C:\\STMNU2\\data', write_to_csv=True)
 
         # Load student
-        student_csv_path = 'C:\\STMNU2\\data\\rdb_format\\student.csv'
+        student_csv_path = 'C:\\STMNU2\\data\\rdb_format\\student.csv' 
         self.student = pd.read_csv(student_csv_path).convert_dtypes()
         self.student.csv_path = student_csv_path
         # Drop the rows where name is completely missing
@@ -59,6 +67,8 @@ class StudentDatabase:
         trial_csv_path = 'C:\\STMNU2\\data\\rdb_format\\trial.csv'
         self.trial = pd.read_csv(trial_csv_path)
         self.trial.csv_path = trial_csv_path
+        for col in ['DATE']:
+            self.trial[col] = pd.to_datetime(self.trial[col], errors='coerce', format='mixed').dt.strftime('%m/%d/%Y')
 
         # Load waitlist students
         wait_csv_path = 'C:\\STMNU2\\data\\rdb_format\\wait.csv'
@@ -76,7 +86,7 @@ class StudentDatabase:
         matches = self.student[(((self.student['FNAME'].str.upper().str.startswith(query['First Name'], na=False)) | (len(query['First Name']) == 0))
                             & ((self.student['LNAME'].str.upper().str.startswith(query['Last Name'], na=False)) | (len(query['Last Name']) == 0)))
                             ].sort_values(by=['LNAME','FNAME'], key=lambda x: x.str.upper()
-                            ).loc[:, ['STUD_ID', 'FNAME','LNAME']
+                            ).loc[:, ['STUDENT_ID', 'FNAME','LNAME']
                             ].fillna(''
                             ).reset_index(drop=True)
         
@@ -84,7 +94,7 @@ class StudentDatabase:
 
     def update_student_info(self, student_id, entry_boxes):
         # Get dataframe index associated with 'student_id'
-        student_idx = self.student[self.student['STUD_ID'] == student_id].index[0]
+        student_idx = self.student[self.student['STUDENT_ID'] == student_id].index[0]
 
         new_values = [entry.get() for entry in entry_boxes.values()]
         new_student_info = pd.Series({k:v for (k,v) in zip(entry_boxes.keys(), new_values)})
@@ -186,7 +196,50 @@ class StudentDatabase:
         # student_csv.to_csv('C:\\STMNU2\\data\\rdb_format\\student.csv', index=False, header=True)
         # guardian_csv.to_csv('C:\\STMNU2\\data\\rdb_format\\guardian.csv', index=False, header=True)
 
-        
+    def update_class_info(self, class_id, entry_boxes):
+        # Get dataframe index associated with 'class_id'
+        class_idx = self.classes[self.classes['CLASS_ID'] == class_id].index[0]
+
+        new_values = [entry.get() for entry in entry_boxes.values()]
+        new_info = pd.Series({k:v for (k,v) in zip(entry_boxes.keys(), new_values)})
+        for col in entry_boxes.keys():
+            if len(new_info[col]) == 0:
+                new_info[col] = 0 if entry_boxes[col].dtype == 'float' else None
+            elif entry_boxes[col].dtype == 'float':
+                new_info[col] = float(new_info[col])
+            elif entry_boxes[col].dtype == 'int':
+                new_info[col] = int(float(new_info[col]))
+            else:
+                new_info[col] = new_info[col].upper()
+
+
+        ## Step 1: Update student info in the Pandas dataframe
+        for col in entry_boxes.keys():
+            self.classes.loc[class_idx, col] = new_info[col]
+
+        ## Step 2: Update student info in original database (DBF file)
+        with self.classes_dbf:
+            class_id_idx = self.classes_dbf.create_index(lambda rec: rec.class_id)
+            # get a list of all matching records
+            match = class_id_idx.search(match=class_id)
+            # should only be one student with that studentno
+            record = match[0]
+            # Focus on this student's record
+            with record:
+                # Loop through each field
+                for field in new_info.keys():
+                    # Get info about this field in the dbf file
+                    field_info = self.classes_dbf.field_info(field)
+                    # Convert date fields to proper format
+                    if str(field_info.py_type) == "<class 'datetime.date'>":
+                        if new_info[field] is not None:
+                            new_info[field] = datetime.strptime(new_info[field], "%m/%d/%Y")
+                    # For this record, if the dbase field does not match the user-entered field,
+                    # update that field in the dbf file (if the field is unchanged, ignore)
+                    if record[field] != new_info[field]:
+                        record[field] = new_info[field]        
+
+
     # The dataframe is sorted chronologically by default. This function will sort the dataframe alphabetically,
     # dealing with the case-sensitivity which is built in to Pandas
     def sort_student_alphabetical(self):
@@ -201,7 +254,7 @@ class StudentDatabase:
         matches = self.classes[(((self.classes['TEACH'] == filters['INSTRUCTOR']) | (len(filters['INSTRUCTOR']) == 0))
                                 & ((self.classes['CLASSNAME'].str.contains(filters['GENDER'])) | (len(filters['GENDER']) == 0))
                                 & ((self.classes['DAYOFWEEK'] == filters['DAY']) | (len(str(filters['DAY'])) == 0))
-                                & ((self.classes['CLASSNAME'].str.contains(filters['LEVEL'])) | (self.classes['CODE'] == filters['LEVEL']) | (len(filters['LEVEL']) == 0)))
+                                & ((self.classes['CLASSNAME'].str.contains(filters['LEVEL'])) | (self.classes['CLASSTIME'].str.contains(filters['LEVEL'])) | (len(filters['LEVEL']) == 0)))
                               ].sort_values(by=['DAYOFWEEK', 'CLASSTIME']
                               ).loc[:, ['CLASS_ID', 'TEACH', 'CLASSTIME', 'CLASSNAME', 'MAX']]
         
