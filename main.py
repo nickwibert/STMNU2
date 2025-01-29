2e81955db97a439d9426254b9eb33657ff369788
Nick Wibert <nickwibert@gmail.com>
2025-01-29 17:40:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def main():
    # Load student info
    database = StudentDatabase(student_dbf_path='C:\\dbase\\gymtek\\STUD00.dbf',
                               student_prev_year_dbf_path='C:\\dbase\\gymtek\\STUD99.dbf',
-                              clsbymon_dbf_path='C:\\dbase\\gymtek\\clsbymon.dbf')
+                              clsbymon_dbf_path='C:\\dbase\\gymtek\\clsbymon.dbf',
+                              update_active=False)
    # Initialize instance of program
    root = gui.STMNU(database)
 
@@ -15,4 +16,4 @@ def main():
 if __name__ == "__main__":
    main()
 
- 
+   
